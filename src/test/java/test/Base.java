1185b92c5167ca099e1cd810ce5e0e59b59bb8b5
MohamedTae <70129250+MohamedTae@users.noreply.github.com>
2020-09-28 12:28:16 +0300

    Update Base.java

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -1,58 +1,37 @@
 package test;
 
-import java.io.*;
-import java.util.*;
-import static org.junit.Assert.*;
-import org.junit.internal.AssumptionViolatedException;
+import static org.junit.Assert.fail;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-public class Base
-{
-
-  public static void run(String args[]) 
-  throws IOException
-  {
-    // create the ping command as a list of strings
-    Base ping = new Base();
-    List<String> commands = new ArrayList<String>();
-    commands.add("ping");
-    commands.add("-c");
-    commands.add("5");
-    commands.add("127.0.0.1");
-    commands.add("grep");
-    commands.add("-e");
-    commands.add("'packet loss'");
-    commands.add("-e");
-    commands.add("'max'");
-
-    ping.doCommand(commands);
-  }
-
-  public void doCommand(List<String> command) 
-  throws IOException
-  {
-    String s = null;
-
-    ProcessBuilder pb = new ProcessBuilder(command);
-    Process process = pb.start();
-
-    BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-    BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-
-    // read the output from the command
-    System.out.println("Here is the standard output of the command:\n");
-    while ((s = stdInput.readLine()) != null)
-    {
-      System.out.println(s);
+public class Base {
+    public static void main(String args[]) {
+        String s;
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec("ping -c10 google.com");
+            BufferedReader br = new BufferedReader(
+                new InputStreamReader(p.getInputStream()));
+            while ((s = br.readLine()) != null){
+                System.out.println(s);
+                if(s.contains("packet loss")){
+                    String mySubString = s.substring(
+                        s.lastIndexOf(", ") + 1, 
+                        s.lastIndexOf("%")
+                    );
+                    Float PacketLoss = Float.valueOf(mySubString);
+                    if(PacketLoss >= 50){
+                        fail("Failed");
+                    }
+                    else{
+                        System.out.println("line: " + PacketLoss);
+                    }
+                }
+            }
+            p.waitFor();
+            //System.out.println ("exit: " + p.exitValue());
+            p.destroy();
+        } catch (Exception e) {}
     }
-
-    // read any errors from the attempted command
-    System.out.println("Here is the standard error of the command (if any):\n");
-    while ((s = stdError.readLine()) != null)
-    {
-      System.out.println(s);
-      fail("ooops");
-    }
-  }
-
 }
