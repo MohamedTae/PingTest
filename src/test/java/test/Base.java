495836240f4c3ad1333002612d5e0222f21aacdf
MohamedTae <70129250+MohamedTae@users.noreply.github.com>
2020-09-30 13:22:46 +0300

    Update Base.java

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -1,11 +1,11 @@
 package test;
 import static org.junit.Assert.*;
-import org.junit.internal.AssumptionViolatedException;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Base {
+    
     public static void main(String args[]) {
         String s;
         Process p;
@@ -15,18 +15,28 @@ public class Base {
                 new InputStreamReader(p.getInputStream()));
             while ((s = br.readLine()) != null){
                 System.out.println(s);
-                String P = String.valueOf(s);
-                GenerateCsv.generateCsvFile("s.csv", P);
                 if(s.contains("packet loss")){
                     s = s.substring(s.indexOf("received, ") + 10);
                     s = s.substring(0, s.indexOf("%"));
                     System.out.println(s);
+                    
                     Float PacketLoss = Float.valueOf(s);
                     if(PacketLoss >= 50){
                         fail("Failed");
                         System.out.println("line: " + PacketLoss);
                     }
-                    
+                }
+                if(s.contains("avg/max/")){
+                    s = s.substring(s.indexOf(" = ") + 3);
+                    //System.out.println(s);
+                    s = s.substring(s.indexOf("/") + 1);
+                    //System.out.println(s);
+                    s = s.substring(s.indexOf("/") + 1);
+                    //System.out.println(s);
+                    s = s.split("/")[0];
+                    //System.out.println(s);
+                    String P = String.valueOf(s);
+                    GenerateCsv.generateCsvFile("s.csv", P);
                 }
             }
             p.waitFor();
