931e8c4ba0c0f96f2cd85d8c5e5d1ffd9490911e
MohamedTae <70129250+MohamedTae@users.noreply.github.com>
2020-09-30 11:56:13 +0300

    Create GenerateCsv.java

---
+++ src/test/java/test/GenerateCsv.java
@@ -0,0 +1,33 @@
+package test;
+import java.io.FileWriter;
+import java.io.IOException;
+ 
+public class GenerateCsv
+{
+   public static void main(String [] args)
+   {
+    generateCsvFile("test.csv","3.3"); 
+   }
+ 
+   public static void generateCsvFile(String sFileName,String max)
+   {
+ try
+ {
+     FileWriter writer = new FileWriter(sFileName,true);
+ 
+     writer.append("Max Ping");
+     writer.append('\n');
+     writer.append(max);
+     writer.append('\n');
+ 
+     //generate whatever data you want
+ 
+     writer.flush();
+     writer.close();
+ }
+ catch(IOException e)
+ {
+      e.printStackTrace();
+ } 
+    }
+}
